9a18c69ee064147811abfd9ac1c2a3659969d2be
mehulbhange <bhangemehul@gmail.com>
2022-09-23 10:10:14 +0530

    ADDED:[Mehul] added function to calculate daily wage

--- employeeWage.js
+++ employeeWage.js
@@ -5,19 +5,18 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHrs = 0;
-empHrs = 4
-empCheck = Math.floor(Math.random() * 10) % 3; 
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME: 
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs = 0;
+function getWorkingHours(empCheck){
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME: 
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
 
+empCheck = Math.floor(Math.random() * 10) % 3; 
+let empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Employee wage :"+ empWage);
