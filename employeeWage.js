cd3eba12b1c01f8e52f1b64d4b2d9fece5bad325
mehulbhange <bhangemehul@gmail.com>
2022-09-25 18:30:18 +0530

    ADDED:[Mehul] added array to store daily emp wages

--- employeeWage.js
+++ employeeWage.js
@@ -1,4 +1,4 @@
-// UC 2
+
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -6,6 +6,8 @@ const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
 const MAX_WORKING_HRS = 160;
+
+let employeeDailyWage = new Array();
 
 function getWorkingHours(empCheck){
     switch (empCheck) {
@@ -17,6 +19,10 @@ function getWorkingHours(empCheck){
             return 0;
     }
 }
+
+function calculateDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOUR;
+}
  
 let empWorkingDays = 0;
 let empWorkingHrs = 0;
@@ -24,8 +30,10 @@ let empWorkingHrs = 0;
 while (empWorkingHrs <= MAX_WORKING_HRS && empWorkingDays < NUM_OF_WORKING_DAYS){
     empWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    empWorkingHrs += getWorkingHours(empCheck);
+    let empHrs = getWorkingHours(empCheck);
+    empWorkingHrs += empHrs;
+    employeeDailyWage.push(calculateDailyWage(empHrs));
 }
 
-let empWage = empWorkingHrs * WAGE_PER_HOUR;
+let empWage = calculateDailyWage(empWorkingHrs);
 console.log("Total Days : "+empWorkingDays + " Total Hrs : "+ empWorkingHrs+ " Employee wage : "+ empWage);
